e471943fe6301755ac96bca77e26b9a19d610fb5
Kateryna <vlkvskatia@gmail.com>
2026-02-04 13:22:32 +0200

    final fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,11 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/about',
+        path: 'about',
         element: <About />,
       },
       {
-        path: '/contact',
+        path: 'contact',
         element: <Contact />,
       }
     ]
